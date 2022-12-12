523c7063df05dea3d278696ecca43d1a687e4401
Te Su <sute515@gmail.com>
2022-12-12 11:33:06 -0800

    Upload source code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,84 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { marked } from 'marked';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+let initialState = `# Preview Your Markdown Syntax
+## See changes live
+Here are **some** of the ways you can *style* your text.
+
+Want inspiration?
+Here is an indented quote:
+> A journey of a thousand miles begins with a single step. -- Lao Tzu
+
+Here's how to do \`inline\` quotes.
+
+And block quotes:
+
+\`\`\`
+let sum = 0;
+for (let i = 0; i < 5; i++) {
+  sum += i;
+}
+\`\`\`
+
+You can create a list.
+
+- Apples
+- Bananas
+- Pears
+
+### [Here](https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax) is a resource to learn more about the markdown syntax.
+
+`;
+
+
+class Markdown extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: initialState
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.clear = this.clear.bind(this);
+    this.reset = this.reset.bind(this);
+  }
+  handleChange(e) {
+    this.setState({
+      input: e.target.value
+    });
+  } 
+  clear() {
+    this.setState({
+      input: ""
+    });
+  }
+  reset() {
+    this.setState({
+      input: initialState
+    });
+  }
+
+  render() {
+    return (
+      <div className = "App">
+        <div className = 'header'>
+          Markdown Previewer
+        </div>
+        <div className = 'button-div'>
+          <button className = 'button' onClick = {this.clear}>Clear</button>
+          <button className = 'button' onClick = {this.reset}>Reset</button>
+        </div>
+        <div className = 'container'>
+          <textarea className = 'editor' value = {this.state.input} onChange = {this.handleChange}>
+          </textarea>
+          <div className = 'previewer' dangerouslySetInnerHTML = {{__html: marked(this.state.input, {breaks: true})}}>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Markdown />);
